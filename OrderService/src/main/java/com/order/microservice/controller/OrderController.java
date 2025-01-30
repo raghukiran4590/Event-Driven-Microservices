c156ed4057037d2bd43469e3134acc2123b445f5
Raghu Kiran <raghukiran4590@gmail.com>
2025-01-29 21:11:42 -0500

    Discovery Service

---
+++ OrderService/src/main/java/com/order/microservice/controller/OrderController.java
@@ -0,0 +1,16 @@
+package com.order.microservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/orders")
+public class OrderController {
+
+    @PostMapping
+    public ResponseEntity<?> createOrder() {
+            return ResponseEntity.ok("Order Created");
+    }
+}
